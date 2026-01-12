6844699cbc4feb342e09089d8060bddc09f54db8
Mikiyas-hs <mikiyashadis160@gmail.com>
2026-01-12 03:20:29 +0100

    feat: add page metadata for title and description (#8)

--- dept-weather-planner/src/app/layout.js
+++ dept-weather-planner/src/app/layout.js
@@ -1,6 +1,11 @@
 import "./globals.css";
 import "../components/Hero/hero.css";
 
+export const metadata = {
+  title: "DEPT® Weather Planner",
+  description:
+    "A smart weather planner that helps you choose the best activities based on current and forecasted weather conditions.",
+};
 
 export default function RootLayout({ children }) {
   return (
