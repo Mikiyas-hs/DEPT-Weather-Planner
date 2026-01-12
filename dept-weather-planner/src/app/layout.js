aad4f4f550f8e1bda106b1a858219497e039ac03
Mikiyas-hs <mikiyashadis160@gmail.com>
2026-01-12 22:53:34 +0100

    feat: add footer to layout
    include footer component in layout.js

--- dept-weather-planner/src/app/layout.js
+++ dept-weather-planner/src/app/layout.js
@@ -2,7 +2,9 @@ import "./globals.css";
 import "../components/Hero/hero.css";
 import "../components/Weather/weather.css";
 import "../components/Things-to-do/things.css"
+import "../components/Footer/footer.css"
 import Hero from "@/components/Hero/hero";
+import Footer from "@/components/Footer/footer";
 export const metadata = {
   title: "DEPT® Weather Planner",
   description:
@@ -14,7 +16,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
       <header><Hero /></header>
-      {children}</body>
+      <main>{children}</main>
+    <Footer />
+    </body>
     </html>
   );
 }
