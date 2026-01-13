efaba24bd850cf4ff5054cd640eab26b5738bb18
Mikiyas-hs <mikiyashadis160@gmail.com>
2026-01-13 13:13:04 +0100

    Chore: add comments and format code
    added comments and improved code formatting for readability.

--- dept-weather-planner/src/components/Weather/weather.jsx
+++ dept-weather-planner/src/components/Weather/weather.jsx
@@ -1,20 +1,21 @@
 export default function Weather({ weather }) {
-    const temp = weather.temperature.temp;
+  const temp = weather.temperature.temp;
 
-    const info = weather.weatherInfo.find(({ minTemp, maxTemp }) => 
-        (minTemp ?? -Infinity) <= temp && temp <= (maxTemp ?? Infinity)
-      );
-      
+//   juiste tekst vinden op basis van temperatuur minTemp/maxTemp
+  const info = weather.weatherInfo.find(
+    ({ minTemp, maxTemp }) =>
+      (minTemp ?? -Infinity) <= temp && temp <= (maxTemp ?? Infinity)
+  );
 
-      return (
-        <section className="weather">
-          <h1 className="weather_temp">{Math.round(temp)}°</h1>
-    
-          <p className="weather_headline">
-            {info.title.replace("{{ CELCIUS }}", temp)}
-          </p>
-    
-          <p className="weather_body">{info.description}</p>
-        </section>
-      );
-    }
+  return (
+    <section className="weather">
+      <h1 className="weather_temp">{Math.round(temp)}°</h1>
+
+      <p className="weather_headline">
+        {info.title.replace("{{ CELCIUS }}", temp)}
+      </p>
+
+      <p className="weather_tekst">{info.description}</p>
+    </section>
+  );
+}
