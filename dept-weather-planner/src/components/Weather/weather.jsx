a6d1d11cf07523935d004900bd71cec9073be280
Mikiyas-hs <mikiyashadis160@gmail.com>
2026-01-13 14:06:18 +0100

    fix: converting temperature to celsius
    Convert fahrenheit to celsius

--- dept-weather-planner/src/components/Weather/weather.jsx
+++ dept-weather-planner/src/components/Weather/weather.jsx
@@ -1,18 +1,24 @@
 export default function Weather({ weather }) {
-  const temp = weather.temperature.temp;
+    const { temp, metric } = weather.temperature;
 
-//   juiste tekst vinden op basis van temperatuur minTemp/maxTemp
+    // coverting temp to cels 
+    const tempC = metric === "FAHRENHEIT" ? (temp - 32) * (5 / 9) : temp;
+
+    // round up temp
+    const c = Math.round(tempC);
+
+    //   juiste tekst vinden op basis van temperatuur minTemp/maxTemp
   const info = weather.weatherInfo.find(
     ({ minTemp, maxTemp }) =>
-      (minTemp ?? -Infinity) <= temp && temp <= (maxTemp ?? Infinity)
+      (minTemp ?? -Infinity) <= c && c <= (maxTemp ?? Infinity)
   );
 
   return (
     <section className="weather">
-      <h1 className="weather_temp">{Math.round(temp)}°</h1>
+      <h1 className="weather_temp">{c}°</h1>
 
       <p className="weather_headline">
-        {info.title.replace("{{ CELCIUS }}", temp)}
+        {info.title.replace("{{ CELCIUS }}", c)}
       </p>
 
       <p className="weather_tekst">{info.description}</p>
