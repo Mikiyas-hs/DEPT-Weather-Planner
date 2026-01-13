529b6dd6944b1d31b087f978b5d1eb9b16eaf216
Mikiyas-hs <mikiyashadis160@gmail.com>
2026-01-13 13:42:52 +0100

    feat: restructure layout (#16)
    Updated homepage structure, moved css imports, and grouped sections for responsive layout

--- dept-weather-planner/src/app/page.js
+++ dept-weather-planner/src/app/page.js
@@ -1,19 +1,26 @@
-import Hero from "../components/Hero/hero";
+import "./page.css";
+import "../components/Hero/hero.css";
+import "../components/Weather/weather.css";
+import "../components/Things-to-do/things.css"
+import "../components/Footer/footer.css"
 import Weather from "../components/Weather/weather";
 import Things from "../components/Things-to-do/things"
-import Footer from "../components/Footer/footer"
+import Hero from "../components/Hero/hero"
 import {getThingsToDo, getWeather} from "../lib/api";
+// import { Children } from "react";
 
 export default async function HomePage() {
   const weather = await getWeather();
   const things = await getThingsToDo();  
   
   return (
-    <main>
-      <Weather weather={weather} />
-      <Things 
+    <main className="home">
+      <section className="hero_section"><Hero /></section>
+      <section className="weather_section"><Weather weather={weather} /></section>
+      <section className="things_section"><Things
       activities={things.activities}
       temp={weather.temperature.temp} />
+      </section>
     </main>
   );
 }
